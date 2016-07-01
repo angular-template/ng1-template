2521706d1b0948bb30f86a9920cfdbc0bd785671
Jeevan James <jeevanjj@yahoo.com>
2016-07-01 14:08:12 +0530

    Removed the old style tsd typings files. Added typings support and updated the .gitignore file to ignore everything under typings except for app.d.ts.template

--- typings/lib.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="angular-ui-router/angular-ui-router.d.ts" />
-/// <reference path="angularjs/angular-animate.d.ts" />
-/// <reference path="angularjs/angular-sanitize.d.ts" />
-/// <reference path="angularjs/angular.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="lodash/lodash.d.ts" />
