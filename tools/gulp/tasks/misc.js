642558a1ee7e5927a04edf15168c1867ebb2a708
Jeevan James <jeevanjj@yahoo.com>
2016-07-27 03:43:12 +0530

    Updated firebase deployment to use production build. Updated clean task to delete .build root folder as well.

--- tools/gulp/tasks/misc.js
+++ tools/gulp/tasks/misc.js
@@ -6,8 +6,12 @@ let $ = require('gulp-load-plugins')({lazy: true});
 let tsks = require('./task-names');
 let utils = require('./utils');
 
+let folders = require('../config/core.folders');
+
 gulp.task(tsks.help, $.taskListing.withFilters(/(_|:)/, task => task === tsks.default || task === tsks.help));
 
 gulp.task(tsks.default, [tsks.dev.serve]);
 
-gulp.task('clean', [tsks.dist.clean, tsks.dev.clean]);
+gulp.task('clean', [tsks.dist.clean, tsks.dev.clean], done => {
+    utils.clean(folders.build, done);
+});
