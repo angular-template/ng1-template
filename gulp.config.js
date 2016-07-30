109e2e50251c1f1c3d06769fc5f19bcde199e0fd
Jeevan James <jeevanjj@yahoo.com>
2016-07-31 03:18:45 +0530

    Big changes: Moved all Gulp functionality to the ng1-template-gulp package. The tools/gulp folder is removed.

--- gulp.config.js
+++ gulp.config.js
@@ -3,11 +3,14 @@
 let utils = require('ng1-template-gulp').utils;
 
 module.exports = function(config) {
+    /**
+     * Create all application modules and add them to the config.modules array in increasing order
+     * of their dependencies to one another.
+     * The common module is added by default, so you do not need to add it if it's needed. If it is
+     * not needed, you'll need to remove it first.
+     */
     config.modules.push(utils.createModule('security'));
-    config.modules.push(utils.createModule('demo', {
-        jsToInject: [
-            'layouts/**/*.js',
-            '**/*.js'
-        ]
-    }));
+    config.modules.push(utils.createModule('demo'));
+
+    // Add your project-specific configurations by adding to or modifying the config object.
 };
