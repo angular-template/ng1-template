8fa165917d76d839e0cffd537eea70cd239a71f7
Jeevan James <jeevanjj@yahoo.com>
2016-08-03 21:26:12 +0530

    Add variable for common module.

--- gulp.config.ts
+++ gulp.config.ts
@@ -6,6 +6,12 @@ let utils: IUtils = require('ng1-template-gulp').utils;
 
 module.exports = function(config: IConfig) {
     /**
+     * The following variables represent aspects of the default configuration.
+     * If you are changing any existing values in the default config, update or delete the variables accordingly.
+     */
+    let commonModule: IModule = config.modules[0];
+
+    /**
      * Create all application modules and add them to the config.modules array in increasing order
      * of their dependencies to one another.
      * The common module is added by default, so you do not need to add it if it's needed. If it is
@@ -15,7 +21,4 @@ module.exports = function(config: IConfig) {
     config.modules.push(utils.createModule('demo'));
 
     // Add your project-specific configurations by adding to or modifying the config object.
-    config.styles.usesLess = true;
-
-    config.styles.injections.unshift(`${config.folders.bower}bootstrap/dist/css/bootstrap.css`);
 };
