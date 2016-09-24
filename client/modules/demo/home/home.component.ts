ee0929d31056c2aa373b38d63f9ecce877882ab9
Jeevan James <jeevanjj@yahoo.com>
2016-09-25 00:18:53 +0530

    Update ng1-template-core.

--- client/modules/demo/home/home.component.ts
+++ client/modules/demo/home/home.component.ts
@@ -1,15 +1,40 @@
 /// <reference path="../../../../typings/index.d.ts"/>
 /// <reference path="../../../../typings/app.d.ts"/>
 
+namespace route {
+}
+
 namespace demo.home {
     export const name: string = 'home';
 
     @Component({
         selector: name
     }, {
-            path: '/home',
+            path: '/home/:userId?my-pref',
             parent: layouts.sample.name
         })
     export class HomeComponent implements ng.IComponentController {
+        /* @ngInject */
+        constructor(private $log: ng.ILogService) {
+        }
+
+        public $onInit(): void {
+            this.$log.log(this.routeParams, this.userId, this.pref);
+        }
+
+        @route.multiple({
+            userId: 'string',
+            '?my-pref': 'string'
+        })
+        public routeParams: {
+            userId: string;
+            myPref: string;
+        };
+
+        @route.param('boolean')
+        public userId: boolean;
+
+        @route.query('string', 'my-pref')
+        public pref: string;
     }
 }
